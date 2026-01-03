89d489f3b409950e228e3b8f449ff29a1ddff0fa
SeniorIntern <tnikhil.official@gmail.com>
2026-01-03 12:33:47 +0400

    Disable telescope file preview when finding files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,28 +1,43 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  tag = 'v0.2.0',
-  cmd = "Telescope",
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  keys = {
-    {
-      '<leader>ff',
-      function() require('telescope.builtin').find_files() end,
-      desc = 'Telescope: Find Files'
-    },
-    {
-      '<leader>fg',
-      function() require('telescope.builtin').live_grep() end,
-      desc = 'Telescope: Live Grep'
-    },
-    {
-      '<leader>fb',
-      function() require('telescope.builtin').buffers() end,
-      desc = 'Telescope: Buffers'
-    },
-    {
-      '<leader>sk',
-      function() require('telescope.builtin').keymaps() end,
-      desc = 'Telescope: Keymappings'
-    },
-  },
+	"nvim-telescope/telescope.nvim",
+	tag = "v0.2.0",
+	cmd = "Telescope",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	keys = {
+		{
+			"<leader>ff",
+			function()
+				require("telescope.builtin").find_files()
+			end,
+			desc = "Telescope: Find Files",
+		},
+		{
+			"<leader>fg",
+			function()
+				require("telescope.builtin").live_grep()
+			end,
+			desc = "Telescope: Live Grep",
+		},
+		{
+			"<leader>fb",
+			function()
+				require("telescope.builtin").buffers()
+			end,
+			desc = "Telescope: Buffers",
+		},
+		{
+			"<leader>sk",
+			function()
+				require("telescope.builtin").keymaps()
+			end,
+			desc = "Telescope: Keymappings",
+		},
+	},
+	opts = {
+		pickers = {
+			find_files = {
+				previewer = false,
+			},
+		},
+	},
 }
